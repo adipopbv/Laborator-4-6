951789845c1781e8f5a7bd839b0dd7a3a41fc0cb
PridesFall08 <adi.pop.bv@gmail.com>
2019-10-25 17:28:27 +0300

    updated import names

--- Cheltuieli_de_familie_rework/main.py
+++ Cheltuieli_de_familie_rework/main.py
@@ -2,11 +2,17 @@ from Cheltuieli_de_familie_rework.UI import IO
 from Cheltuieli_de_familie_rework.UI import Graphics
 from Cheltuieli_de_familie_rework import Commands
 
+from Cheltuieli_de_familie_rework.Tests import Test_Repo
+from Cheltuieli_de_familie_rework.Tests import Test_Expenses
+from Cheltuieli_de_familie_rework.Tests import Test_Validator
+
 def RunAllTests():
     """
     runs all asserting tests in the project
     """
-    pass
+    Test_Repo.RunAllTests()
+    Test_Expenses.RunAllTests()
+    Test_Validator.RunAllTests()
 
 def RunApplication():
     """
