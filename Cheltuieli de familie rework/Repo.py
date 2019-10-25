0eda0d4be406c9155f94af58570f4b02a63290ed
PridesFall08 <adi.pop.bv@gmail.com>
2019-10-25 13:47:41 +0300

    added repo

---
+++ Cheltuieli de familie rework/Repo.py
@@ -0,0 +1,11 @@
+expenseRepo = []
+
+def AddToRepo(repo, item):
+    """
+    adds an item to a repository
+    
+    Args:
+        repo (list): a repository to add to
+        item (any type): an item to be added
+    """
+    repo.append(item)
