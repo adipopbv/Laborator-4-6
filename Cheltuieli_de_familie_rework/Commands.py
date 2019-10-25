2e854f25c81d6d5f46bfa203b0db5cee45631dc7
PridesFall08 <adi.pop.bv@gmail.com>
2019-10-25 16:32:23 +0300

    added update expanse functionality

--- Cheltuieli_de_familie_rework/Commands.py
+++ Cheltuieli_de_familie_rework/Commands.py
@@ -3,7 +3,7 @@ from Cheltuieli_de_familie_rework import Repo
 
 commands = {
     1: AddNewExpense,
-    #2: UpdateExpense,
+    2: UpdateExpense,
     #6: SearchExpensesGreaterThanAmmount,
     #9: TotalAmmountForGivenCategory,
     #13: EraseExpensesOfGivenCategory,
@@ -20,5 +20,17 @@ def AddNewExpense():
     try:
         expense = IO.GetExpense()
         Repo.AddToRepo(Repo.expenseRepo, expense)
+    except Exception as ex:
+        IO.OutputException(ex)
+
+def UpdateExpense():
+    """
+    updates an expense to a new state
+    """ 
+    try:
+        originalExpense = IO.GetExpense()
+        IO.OutputText("De inlocuit cu...")
+        updatedExpense = IO.GetExpense()
+        Repo.SwapInRepo(Repo.expenseRepo, originalExpense, updatedExpense)
     except Exception as ex:
         IO.OutputException(ex)
--- Cheltuieli_de_familie_rework/Repo.py
+++ Cheltuieli_de_familie_rework/Repo.py
@@ -1,5 +1,24 @@
 expenseRepo = []
 
+def IndexOfItemInRepo(repo, item):
+    """
+    searches the index of an item in a repository
+    
+    Args:
+        repo (list): a repository of items
+        item (any type): an item to search
+    
+    Raises:
+        Exception: inexistent item in repo
+    
+    Returns:
+        int: index of item in repo
+    """
+    for repoItem in repo:
+        if repoItem == repo:
+            return repo.index(repoItem)
+    raise Exception("Entitate inexistenta in lista!")
+
 def AddToRepo(repo, item):
     """
     adds an item to a repository
@@ -7,5 +26,15 @@ def AddToRepo(repo, item):
     Args:
         repo (list): a repository to add to
         item (any type): an item to be added
+
+    Raises:
+        Exception: expense not found in repo
     """
     repo.append(item)
+
+def SwapInRepo(repo, originalExpense, updatedExpense):
+
+    try:
+        expenseRepo[IndexOfItemInRepo(expenseRepo, originalExpense)] = updatedExpense
+    except Exception as ex:
+        raise Exception(ex)
