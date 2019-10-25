bde05937fabf0a21ddfc16c7f094708a38f03424
PridesFall08 <adi.pop.bv@gmail.com>
2019-10-25 13:34:39 +0300

    added commands

---
+++ Cheltuieli de familie rework/Commands.py
@@ -0,0 +1,24 @@
+from UI import IO
+import Repo
+
+commands = {
+    1: AddNewExpense,
+    #2: UpdateExpense,
+    #6: SearchExpensesGreaterThanAmmount,
+    #9: TotalAmmountForGivenCategory,
+    #13: EraseExpensesOfGivenCategory,
+    #16: ExitApplication
+}
+
+def DoCommandWithId(commandId):
+    commands[commandId]()
+
+def AddNewExpense():
+    """
+    adds a new expense to the expenses repository
+    """
+    try:
+        expense = IO.GetExpense()
+        Repo.AddToList(Repo.expenseRepo, expense)
+    except Exception as ex:
+        IO.OutputException(ex)
